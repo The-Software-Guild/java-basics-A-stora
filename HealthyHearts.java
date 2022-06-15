6de83225fc27ecf6ce2e73c8286fe76078ebcb5c
Alex <alexjtyrrell@yahoo.co.uk>
2022-06-15 09:10:56 +0100

    first complete version

--- HealthyHearts.java
+++ HealthyHearts.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class HealthyHearts {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("What is your age?");
+
+        String age = input.next();
+
+        while(true){
+            try {
+                Integer.parseInt(age);
+                break;
+            }catch(Exception e){
+                System.out.println("That is not a number, please try again");
+                age = input.next();
+            }
+        }
+        int ageInt = Integer.parseInt(age);
+        int maxRate = 220 - ageInt;
+        int rangeLow = (int)(Math.round(maxRate*0.5));
+        int rangeHigh = (int)(Math.round(maxRate*0.85));
+        System.out.println("Your maximum heart rate should be "+maxRate+" beats per minute");
+        System.out.println("Your target HR zone is "+rangeLow+" - "+rangeHigh+" beats per minute");
     }
 }
